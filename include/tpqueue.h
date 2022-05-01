77cfa5528404fe98018e6e926e731861cef0bb0a
Abubyakirov-Timur <99200506+Abubyakirov-Timur@users.noreply.github.com>
2022-05-01 15:43:03 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,57 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+struct ITEM {
+     T value;
+     ITEM* next;
+  };
+  ITEM* first, * last;
+}
+  
+  public:
+  TPQueue() :head(nullptr), tail(nullptr) {}
+  T pop() {
+    if (head) {
+      ITEM* temp = head;
+      head = head->next;
+      return temp->data;
+    }
+    T tmp;
+    tmp.prior = 0;
+    tmp.ch = ' ';
+    return tmp;
+  }
+  void push(T value) {
+    ITEM* tmp = nullptr;
+    ITEM* it = create(value);
+    ITEM* temp = head;
+    if (head && tail) {
+       while (temp && (temp->data).prior >= value.prior) {
+         tmp = temp;
+         temp = temp->next;
+       }
+       if (temp == head && ((head->data).prior == (it->data).prior)) {
+         it->next = head->next;
+         head->next = it;
+       } else if (temp == head && ((head->data).prior >= (it->data).prior)) {
+         it->next = head->next;
+         head->next = it;
+       } else if (temp == head && ((head->data).prior < (it->data).prior)) {
+         it->next = head;
+         head = it;
+       } else if (!temp) {
+         tail->next = it;
+         tail = it;
+       } else {
+         tmp->next = it;
+         it->next = temp;
+       }
+    } else {
+      head = create(value);
+      tail = head;
+    }
+  }
 };
 
 struct SYM {
